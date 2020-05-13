0c80376e7ccb31dad0013310548e1894266be6d9
apascualro <57284761+apascualro@users.noreply.github.com>
2020-05-13 11:18:23 +0200

    rutas gnericas con / delante

--- Vistas/Header/header.php
+++ Vistas/Header/header.php
@@ -9,34 +9,13 @@
 
 	<!-- CSS -->
 
-	<link rel="stylesheet" type="text/css" href="<?php 
-		if (file_exists("../../Vistas/assets/css/bootstrap.min.css")){echo '../../Vistas/assets/css/bootstrap.min.css';}	
-		if (file_exists("../Vistas/assets/css/bootstrap.min.css")){echo '../Vistas/assets/css/bootstrap.min.css';}	
-		if (file_exists("Vistas/assets/css/bootstrap.min.css")){echo 'Vistas/assets/css/bootstrap.min.css';
-	}?> ">	
-	<link rel="stylesheet" type="text/css" href="<?php 
-		if (file_exists("../../Vistas/assets/css/style01.css")){echo '../../Vistas/assets/css/style01.css';}	
-		if (file_exists("../Vistas/assets/css/style01.css")){echo '../Vistas/assets/css/style01.css';}	
-		if (file_exists("Vistas/assets/css/style01.css")){echo 'Vistas/assets/css/style01.css';
-	}?> ">
-	<link rel="stylesheet" type="text/css" href="<?php 
-		if (file_exists("../../Vistas/assets/css/style02.css")){echo '../../Vistas/assets/css/style02.css';}
-		if (file_exists("../Vistas/assets/css/style02.css")){echo '../Vistas/assets/css/style02.css';}
-		if (file_exists("Vistas/assets/css/style02.css")){echo 'Vistas/assets/css/style02.css';
-	}?> ">
+	<link rel="stylesheet" type="text/css" href="/Vistas/assets/css/bootstrap.min.css">	
+	<link rel="stylesheet" type="text/css" href="/Vistas/assets/css/style01.css">
+	<link rel="stylesheet" type="text/css" href="/Vistas/assets/css/style02.css">
 
 	<!-- JS -->
-	<script src="<?php 
-		if (file_exists('../../Vistas/assets/js/jquery.min.js')){echo '../../Vistas/assets/js/jquery.min.js';}	
-		if (file_exists('../Vistas/assets/js/jquery.min.js')){echo '../Vistas/assets/js/jquery.min.js';}	
-		if (file_exists('Vistas/assets/js/jquery.min.js')){echo 'Vistas/assets/js/jquery.min.js';
-	}?> ">		
-	</script>	
-	<script src="<?php 
-		if (file_exists('../../Vistas/assets/js/bootstrap.min.js')){echo '../../Vistas/assets/js/bootstrap.min.js';}	
-		if (file_exists('../Vistas/assets/js/bootstrap.min.js')){echo '../Vistas/assets/js/bootstrap.min.js';}	
-		if (file_exists('Vistas/assets/js/bootstrap.min.js')){echo 'Vistas/assets/js/bootstrap.min.js';
-	}?> ">		
+	<script src="/Vistas/assets/js/jquery.min.js"></script>	
+	<script src="/Vistas/assets/js/bootstrap.min.js">		
 	</script>	
 
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
